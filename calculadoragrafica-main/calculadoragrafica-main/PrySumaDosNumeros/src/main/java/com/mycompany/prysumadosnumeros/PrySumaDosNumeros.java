92d828fd47464f65970d6c31f71a7ce4081cea12
DenisseVillalba <denissevillalbaproa@gmail.com>
2025-01-14 15:31:50 -0500

    Add files via upload

---
+++ calculadoragrafica-main/calculadoragrafica-main/PrySumaDosNumeros/src/main/java/com/mycompany/prysumadosnumeros/PrySumaDosNumeros.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.prysumadosnumeros;
+
+
+public class PrySumaDosNumeros {
+
+    public static void main(String[] args) {
+      PantallaSuma pant =new PantallaSuma();
+      
+      pant.setVisible(true); //metodo para que se centre 
+      pant.setLocationRelativeTo(null);
+    }
+    }
+
