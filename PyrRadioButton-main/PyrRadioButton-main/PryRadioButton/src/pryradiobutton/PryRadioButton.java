4cdb9ad7c307ab1c1e821c62ba540e8e41e26586
DenisseVillalba <denissevillalbaproa@gmail.com>
2025-01-15 18:32:35 -0500

    Add files via upload

---
+++ PyrRadioButton-main/PyrRadioButton-main/PryRadioButton/src/pryradiobutton/PryRadioButton.java
@@ -0,0 +1,12 @@
+package pryradiobutton;
+
+import View.OperacionesVista;
+
+public class PryRadioButton {
+
+    public static void main(String[] args) {
+        OperacionesVista op=new OperacionesVista();
+        op.setVisible(true);
+    }
+    
+}
