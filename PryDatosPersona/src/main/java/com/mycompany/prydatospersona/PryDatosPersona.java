6fbbd442f6044cfe806758f161e31a95a1fe27a0
DenisseVillalba <denissevillalbaproa@gmail.com>
2025-01-17 11:09:58 -0500

    Add files via upload

---
+++ PryDatosPersona/src/main/java/com/mycompany/prydatospersona/PryDatosPersona.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.prydatospersona;
+
+import Vista.DatosPersonaVista;
+
+public class PryDatosPersona {
+
+    public static void main(String[] args) {
+        DatosPersonaVista pv= new DatosPersonaVista();
+        pv.setVisible(true);
+        
+    }
+}
