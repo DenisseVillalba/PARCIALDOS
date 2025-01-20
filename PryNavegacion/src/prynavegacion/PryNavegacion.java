04d65f16e65414482b658eb72a6bb4b6fdca36d0
DenisseVillalba <denissevillalbaproa@gmail.com>
2025-01-20 10:57:43 -0500

    Add files via upload

---
+++ PryNavegacion/src/prynavegacion/PryNavegacion.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prynavegacion;
+
+import Vista.PantallaPrincipal;
+
+/**
+ *
+ * @author Laboratorios DCCO
+ */
+public class PryNavegacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PantallaPrincipal pantallaPrincipal = new PantallaPrincipal();
+        pantallaPrincipal.setVisible(true);
+    }
+    
+}
